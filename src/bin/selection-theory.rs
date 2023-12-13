7d1a80c30fe13a17eacb782acfa8e6b37ac3257b
Naiker <yinho999@gmail.com>
2023-12-13 21:47:31 +0000

    Added select! macro theory as custom implementation

---
+++ src/bin/selection-theory.rs
@@ -0,0 +1,40 @@
+use tokio::sync::oneshot;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+struct MySelect {
+    rx1: oneshot::Receiver<&'static str>,
+    rx2: oneshot::Receiver<&'static str>,
+}
+
+impl Future for MySelect {
+    type Output = ();
+    // In the actual select! macro, it includes additional functionality like randomly selecting between branches to pool first
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        if let Poll::Ready(val) = Pin::new(&mut self.rx1).poll(cx) {
+            println!("rx1 completed first with {:?}", val);
+            return Poll::Ready(());
+        }
+
+        if let Poll::Ready(val) = Pin::new(&mut self.rx2).poll(cx) {
+            println!("rx2 completed first with {:?}", val);
+            return Poll::Ready(());
+        }
+
+        Poll::Pending
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx1, rx1) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
+
+    // use tx1 and tx2
+
+    MySelect {
+        rx1,
+        rx2,
+    }.await;
+}
